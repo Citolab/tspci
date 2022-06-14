947a084da5dfbf5fe9225188e748d33b02c8e941
patrick <patrick.deklein@cito.nl>
2022-06-15 00:24:36 +0200

    remove unused methods

--- lib/tspci-questify/types/index.d.ts
+++ lib/tspci-questify/types/index.d.ts
@@ -13,25 +13,25 @@ export interface CES {
    */
   getResponse: () => string;
 
-  /** @access public
-   * Gets an array of resolved manifest media URLs.
-   * @method getMedia
-   * @deprecated not really deprecated, but bundled ci does not contain media ever again
-   */
-  getMedia: () => [];
+  // /** @access public
+  //  * Gets an array of resolved manifest media URLs.
+  //  * @method getMedia
+  //  * @deprecated not really deprecated, but bundled ci does not contain media ever again
+  //  */
+  // getMedia: () => [];
 
-  /** 
-   * @access public
-   * @deprecated not really deprecated, but bundled ci does not contain media ever again
-   * Convenience function to adjust the height of the generated iframe 
-   * to the current height of the custom interaction’s content height;
-   * if that content’s visual size exceeds its layout box (e.g. by using box-shadow), 
-   * an optional vertical margin can be specified.
-   * @method setStageHeight
-   * @arg NumberverticalMargin
-   * @return void
-   */
-  setStageHeight: ([NumberverticalMargin]) => void;
+  // /** 
+  //  * @access public
+  //  * @deprecated not really deprecated, but bundled ci does not contain media ever again
+  //  * Convenience function to adjust the height of the generated iframe 
+  //  * to the current height of the custom interaction’s content height;
+  //  * if that content’s visual size exceeds its layout box (e.g. by using box-shadow), 
+  //  * an optional vertical margin can be specified.
+  //  * @method setStageHeight
+  //  * @arg NumberverticalMargin
+  //  * @return void
+  //  */
+  // setStageHeight: ([NumberverticalMargin]) => void;
 }
 
 export interface CI<ConfigProperties> {
