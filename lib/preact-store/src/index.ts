08a03cca2203daab17fcd5b99bdae5af0fcbfe50
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2023-05-20 08:09:24 +0200

    refactored store

--- lib/preact-store/src/index.ts
+++ lib/preact-store/src/index.ts
@@ -1,126 +1,161 @@
-import { useState, useEffect } from "preact/hooks";
+// a type action with generic T for the payload where T has a property: type
+import { useEffect, useState } from 'preact/hooks';
+export type Action<P> = {
+  type: string;
+  payload: P;
+  timestamp?: number;
+};
 
-let globalState = {};
-let listeners = [];
-let actions: IAction<unknown, unknown, unknown>[] = [];
-let actionSubscription = undefined;
-let triggerReplay = 0;
+export type GenericT<T> = T & { type: string; timestamp?: number };
 
-export function useStore<S, A>(
-  callback?: (actionIdentifier: A, payload) => void
-): [
-  S,
-  <P>(actionIdentifier: A, payload: P) => void,
-  (
-    action?: {
-      type: string;
-      payload: unknown;
-      timestamp?: number;
-    }[],
-    delayMs?: number
-  ) => Promise<void>
-] {
-  const setState = useState(globalState)[1];
+export type Listener<T> = (state: T) => void;
+export type Reducer<T, P> = (state: T, payload: P) => T;
 
-  useEffect(() => {
-    if (triggerReplay > 0) {
-      replay((globalState as any).log, 0);
+
+export interface IStore<T> {
+  getState(): T;
+  getActions(): Action<unknown>[];
+  dispatch<P>(action: Action<P>): void;
+  restoreState(state: T, actions: Action<unknown>[]): void;
+  replay(actions: Action<unknown>[], config?: { speed?: number; animate?: boolean; }): Promise<void>;
+  reset(): void;
+  addReducer<P>(type: string, reducer: Reducer<T, P>): void;
+  subscribe(listener: Listener<T>): void;
+  subscribeActions(listener: Listener<Action<unknown>>): void;
+  unsubscribeAll(): void;
+  unsubscribe(listener: Listener<T>): void;
+}
+
+export class Store<T> implements IStore<T> {
+  private state: T;
+  private listeners: Listener<T>[] = [];
+  private actionListeners: Listener<Action<unknown>>[] = [];
+  private actions: Action<unknown>[] = [];
+  private reducers: { [key: string]: Reducer<T, any> } = {}
+
+  constructor(private initialState: T, restoreData?: {
+    state: T;
+    actions: Action<unknown>[];
+  }) {
+    this.state = restoreData?.state || initialState;
+    if (restoreData?.actions) {
+      this.state = initialState;
+      this.actions = restoreData.actions;
+      for (const action of this.actions) {
+        this.state = this.reduce(this.state, action);
+      }
     }
-  }, [triggerReplay]);
+
+  }
+
+  getState(): T {
+    return this.state;
+  }
+
+  getActions(): Action<unknown>[] {
+    return this.actions;
+  }
+
+  dispatch<P>(payload: Action<P>): void {
+    const timestamp = Date.now();
+    const payloadWithTimestamp = { ...payload, timestamp };
+    this.actions.push(payloadWithTimestamp);
+    this.state = this.reduce(this.state, payloadWithTimestamp);
+    this.notifyListeners(payloadWithTimestamp);
+  }
+
+  reset(): void {
+    this.state = this.initialState;
+    this.actions = [];
+  }
+
+  restoreState(state: T, actions: Action<unknown>[]): void {
+    this.state = state;
+    this.actions = actions;
+  }
+
+
+  async replay(actions: Action<unknown>[], config?: { until?: number; speed?: number; animate?: boolean }): Promise<void> {
+    this.reset();
+    this.actions = [];
+    let previousTimestamp = 0;
+   
+    // if config.until is not null or undefined but accept 0
+    if (config?.until != null) {
+      actions = actions.slice(0, config.until);   
+    }
+
+    
+    for (const action of actions) {
+      // get the delay between the previous action and this action in milliseconds
+      let delay = config?.animate === false ? 0 : !previousTimestamp ? 0 : action.timestamp - previousTimestamp;
+
+      if (config?.speed) {
+        delay /= config.speed;
+      }
+      await this.timeout(delay);
+      this.actions.push(action);
+      this.state = this.reduce(this.state, action);
+      this.notifyListeners(action);
+      previousTimestamp = action.timestamp;
+    }
+  }
+
+  subscribe(listener: Listener<T>): void {
+    this.listeners.push(listener);
+    // Immediately notify the listener with the current state
+    listener(this.state);
+  }
+
+  subscribeActions(listener: Listener<Action<unknown>>): void {
+    this.actionListeners.push(listener);
+  }
+
+  unsubscribe(listener: Listener<T>): void {
+    const index = this.listeners.indexOf(listener);
+    if (index !== -1) {
+      this.listeners.splice(index, 1);
+    }
+  }
+
+  unsubscribeAll(): void {
+    this.listeners = [];
+  }
+
+  addReducer<P>(type: string, reducer: Reducer<T, P>): void {
+    this.reducers[type] = reducer;
+  }
+
+  private reduce(state: T, action: Action<unknown>): T {
+    const reducer = this.reducers[action.type];
+    return reducer ? reducer(state, action.payload) : state;
+  }
+
+  private notifyListeners(action: Action<unknown>): void {
+    for (const listener of this.listeners) {
+      listener(this.state);
+    }
+    for (const listener of this.actionListeners) {
+      listener(action);
+    }
+    console.log('Action:', action);
+  }
+
+  private timeout = (ms: number) => new Promise(res => setTimeout(res, ms));
+}
+
+export function useStore<T>(store: IStore<T>): T {
+  const [state, setState] = useState(store.getState());
 
   useEffect(() => {
-    listeners.push(setState);
-    return () => {
-      listeners = listeners.filter((li) => li !== setState);
+    const listener: Listener<T> = (newState: T) => {
+      setState(newState);
     };
-  }, [setState]);
+    store.subscribe(listener);
+    return () => {
+      store.unsubscribe(listener);
+    };
+  }, [store]);
 
-  function dispatch<P>(actionIdentifier: A, payload: P, bypasslisteners = false) {
-    const typeActions = actions as IAction<S, unknown, A>[];
-    const action = typeActions.find((a) => a.type.toString() === actionIdentifier.toString());
-    // get date now in ms
-    const timestamp = new Date().getTime();
-    // payload = { ...payload, timestamp };
-    callback && callback(action.type, payload);
-    const newState = action.action(globalState as S, payload);
-
-    globalState = { ...globalState, ...newState };
-
-    if (!bypasslisteners) {
-      if (actionSubscription) {
-        actionSubscription({
-          type: actionIdentifier,
-          payload: payload,
-          timestamp,
-        });
-      }
-    }
-
-    for (const listener of listeners) {
-      listener(globalState);
-    }
-  }
-
-  async function replay(replayActions: { type: string; payload: unknown; timestamp?: number }[], speedDivider: number = 1) {
-    for (const action of replayActions) {
-      if (action.timestamp) {
-        // get difference the action.timestamp and the previous action.timestamp
-        const delayMs =
-          action.timestamp - (replayActions[replayActions.indexOf(action) - 1]?.timestamp || action.timestamp);
-        if (speedDivider > 0) {
-          if (speedDivider) {
-            await timeout(delayMs / speedDivider);
-          } else {
-            await timeout(delayMs);
-          }
-        }
-      }
-      dispatch(action.type as unknown as A, action.payload, true);
-    }
-  }
-
-  return [globalState as S, dispatch, replay];
-}
-
-function timeout(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export function cleanupStore() {
-  globalState = {};
-  listeners = [];
-  actions = [];
-}
-
-export interface Store<S> {
-  cleanup: () => void;
-  getState: () => S;
-  replay: (actions: IAction<unknown, unknown, unknown>[]) => void;
-}
-
-export function initStore<S>(
-  userActions: IAction<S, unknown, unknown>[],
-  initialState: S,
-  subscribe?: (action) => void
-): Store<S> {
-  if (initialState) {
-    globalState = { ...globalState, ...initialState };
-  }
-  actions = [...new Set([...actions, ...userActions])] as IAction<unknown, unknown, unknown>[];
-
-  actionSubscription = subscribe;
-  return {
-    getState: () => globalState as S,
-    cleanup: () => cleanupStore(),
-    replay: (logActions: IAction<unknown, unknown, unknown>[]) => {
-      // globalState = {...initialState};
-      globalState = { ...initialState, log: logActions };
-      triggerReplay++;
-    },
-  };
-}
-
-export interface IAction<S, P, A> {
-  type: A;
-  action: (currentState: S, payload: P) => S;
+  return state;
 }
